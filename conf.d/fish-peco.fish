e6175bfed69a814dec5c8c84b119676cbe92dfd6
shota-tsuji <shotatuzi@gmail.com>
2022-08-28 13:53:57 +0900

    feat: set history keybind

--- conf.d/fish-peco.fish
+++ conf.d/fish-peco.fish
@@ -1,5 +1,6 @@
 alias repo="peco_select_ghq"
 alias r="peco_select_ghq"
 
-# bind: Control + R
+# See details on https://fishshell.com/docs/current/cmds/bind.html
+bind \cR 'peco_select_history'
 bind \c[ 'peco_select_ghq'
