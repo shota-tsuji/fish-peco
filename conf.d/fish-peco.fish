a7d4b5a230281999266f0367b5846d4260ac9751
shota-tsuji <shotatuzi@gmail.com>
2022-08-28 14:09:21 +0900

    feat: add function to move recentry directory.

--- conf.d/fish-peco.fish
+++ conf.d/fish-peco.fish
@@ -2,5 +2,6 @@ alias repo="peco_select_ghq"
 alias r="peco_select_ghq"
 
 # See details on https://fishshell.com/docs/current/cmds/bind.html
-bind \cR 'peco_select_history'
+bind \cr 'peco_select_history'
 bind \c[ 'peco_select_ghq'
+bind \co 'peco_select_recent_directory'
---
+++ functions/peco_select_recent_directory.fish
@@ -0,0 +1,17 @@
+function peco_select_recent_directory
+  set -l query (commandline)
+  if test -n $query
+    set peco_flags --query "$query"
+  end
+
+  # Using sed, digits and spaces are shulinked.
+  # ^ is string head
+  # [0-9]\+ is multiple digits
+  # \s\+ is multiple spaces
+  cdr -l | sed 's/^[0-9]\+\s\+//' | peco --prompt="cdr >" $peco_flags | read line
+
+  if test $line
+    cd $line
+    commandline -f repaint
+  fi
+end
